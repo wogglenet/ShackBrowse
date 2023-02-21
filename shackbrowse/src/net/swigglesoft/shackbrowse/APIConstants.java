212ff02517a172515bc5183eecdbff4636c8da54
StimpyDad <stimpy.dad@doesntexist.com>
2023-02-21 09:26:32 -0500

    Check in Imgur auth URL change

--- shackbrowse/src/net/swigglesoft/shackbrowse/APIConstants.java
+++ shackbrowse/src/net/swigglesoft/shackbrowse/APIConstants.java
@@ -4,7 +4,7 @@ package net.swigglesoft.shackbrowse;
 public class APIConstants {
     public static String MY_IMGUR_CLIENT_ID = "placeholder";
     public static String MY_IMGUR_CLIENT_SECRET = "placeholder";
-    public static String MY_IMGUR_REDIRECT_URL = "placeholder";
+    public static String MY_IMGUR_REDIRECT_URL = "shackbrowse://imgur_auth";
     public static String TWITTER_CONSUMER_KEY = "placeholder";
     public static String TWITTER_CONSUMER_SECRET = "placeholder";
     public static String SHACKNEWS_LOL_API_KEY = "placeholder";
